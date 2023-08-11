fee9c1e8acfc65c9c32059474b829f6c71acdf09
Dhanushbharanika <119720388+Dhanushbharanika@users.noreply.github.com>
2023-08-11 15:58:45 +0530

    a =int(input()) b =int(input()) M=a+b A=a-b N=a*b O=a//b J=a%b print(f"Sum:{M}") print(f"Difference:{A}") print(f"Product:{N}") print(f"Quotient:{O}") print(f"Remainder:{J}")

---
+++ Arithmetic_operations.py
@@ -0,0 +1,12 @@
+a =int(input())
+b =int(input())
+M=a+b
+A=a-b
+N=a*b
+O=a//b
+J=a%b
+print(f"Sum:{M}")
+print(f"Difference:{A}")
+print(f"Product:{N}")
+print(f"Quotient:{O}")
+print(f"Remainder:{J}")

80588cba6ea934f632aacc54f2ff43c0e6397aab
Dhanushbharanika <119720388+Dhanushbharanika@users.noreply.github.com>
2023-11-03 15:31:39 +0530

    r,c=map(int,input().split()) mat=[] for i in range(r):     in_lt=list(map(int,input().split()))     mat.append(in_lt) s=0 for in_lt in mat:     for ev_val in in_lt:         s+=ev_val print(s)

---
+++ Sum_of_all_elements_in_a_matrix.py
@@ -0,0 +1,10 @@
+r,c=map(int,input().split())
+mat=[]
+for i in range(r):
+    in_lt=list(map(int,input().split()))
+    mat.append(in_lt)
+s=0
+for in_lt in mat:
+    for ev_val in in_lt:
+        s+=ev_val
+print(s)
